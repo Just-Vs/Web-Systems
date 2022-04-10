bb187b8cb2ae3d513bc924cf45ab012bbf80ba5f
V <virmantasss@gmail.com>
2022-04-10 07:06:01 +0100

    Forms created and added html

---
+++ forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, FileField
+from wtforms.validators import DataRequired, Length, Email
+from flask_wtf.file import FileAllowed, DataRequired, FileField
+
+
+class Register(FlaskForm):
+    username = StringField('Username')
+    email = StringField('Email')
+    password = PasswordField('Password')
+    confirm_password = PasswordField('Confirm Password')
+    submit = SubmitField('Sign Up')
+
+
+class Login(FlaskForm):
+    username = StringField('Username')
+    email = StringField('Email')
+    password = PasswordField('Password')
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password')
+    confirm_password = PasswordField('Confirm Password')
+    submit = SubmitField('Reset Password')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+class Store(FlaskForm):
+    username = StringField('Username')
+    photo = FileField("photo", validators=[FileAllowed(['jpg','jpeg','png'])])
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template,flash, redirect, url_for, session
+from forms import Register, Login
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "537a1b62d2a95fc74624e1db5c24ca78"
 
 @app.route('/')
-def welcome():
+def home():
     return render_template('main.html')
+
+@app.route('/register')
+def registerPage():
+    form = Register()
+    return render_template('register.html', form = form)
+
+
+@app.route('/login')
+def loginPage():
+    form = Login()
+    return render_template('login.html', form = form)
+
 if __name__ == '__main__':
     app.run(debug=True)
