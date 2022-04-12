54a5e1bceb08b33c9def43c8e7ac74eadb130c7b
V <virmantasss@gmail.com>
2022-04-12 08:33:29 +0100

    created account template and minor visual changes

--- app.py
+++ app.py
@@ -10,18 +10,32 @@ def home():
     return render_template('main.html')
 
 @app.route('/register')
-def registerPage():
+def registerpage():
     form = Register()
     return render_template('register.html', form = form)
 
 
 @app.route('/login')
-def loginPage():
+def loginpage():
     form = Login()
     return render_template('login.html', form = form)
 
 @app.route('/blog')
 def blogpage():
     return render_template('blog.html')
+
+@app.route('/store')
+def storepage():
+    return render_template('shop.html')
+
+@app.route('/accountp')
+def accountpage():
+    return render_template('account.html')
+
+@app.route('/accounts')
+def accountpage2():
+    return render_template('account2.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
